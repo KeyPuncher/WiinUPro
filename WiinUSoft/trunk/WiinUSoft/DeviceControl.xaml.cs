e58f2d3d5b1cf63cebaa1d027feff510fd70fae9
jkeys <jkeys@367c74e7-bc52-1c41-9f2d-32f4c5dc80c3>
2014-09-22 04:58:25 +0000

    Added layout for Device Controls.

---
+++ WiinUSoft/trunk/WiinUSoft/DeviceControl.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using NintrollerLib;
+
+namespace WiinUSoft
+{
+    public partial class DeviceControl : UserControl
+    {
+        private Nintroller device;
+        internal Nintroller Device
+        {
+            get { return device; }
+            set
+            {
+                device = value;
+
+                if (device != null)
+                {
+                    device.ExtensionChange += device_ExtensionChange;
+                    device.StateChange += device_StateChange;
+                }
+            }
+        }
+        internal bool Connected
+        {
+            get
+            {
+                if (device == null)
+                    return false;
+
+                return device.Connected;
+            }
+        }
+
+        private ControllerType deviceType = ControllerType.Wiimote;
+        internal ControllerType DeviceType { get; private set; }
+
+        public DeviceControl()
+        {
+            InitializeComponent();
+        }
+
+        public DeviceControl(Nintroller nintroller) : this()
+        {
+            Device = nintroller;
+        }
+
+        void device_ExtensionChange(object sender, ExtensionChangeEventArgs e)
+        {
+            DeviceType = e.Extension;
+        }
+
+        void device_StateChange(object sender, StateChangeEventArgs e)
+        {
+            throw new NotImplementedException();
+            
+            if (DeviceType == ControllerType.ProController)
+            {
+                // TODO: Pro Controller Reading
+            }
+            else if (DeviceType == ControllerType.BalanceBoard)
+            {
+                // TODO: Balance Board Reading
+            }
+            else
+            {
+                // TODO: Wiimote & extension Reading
+                switch (DeviceType)
+                {
+                    case ControllerType.Nunchuk:
+                    case ControllerType.NunchukB:
+                        // TODO: Nunchuck Reading
+                        break;
+
+                    case ControllerType.ClassicController:
+                        // TODO: Classic Controller Reading
+                        break;
+
+                    case ControllerType.ClassicControllerPro:
+                        // TODO: Classic Controller Pro Reading
+                        break;
+                        
+                    case ControllerType.MotionPlus:
+                        // TODO: Motion Plus Reading
+                        break;
+
+                        // TODO: Musical Extension readins
+                }
+            }
+        }
+    }
+}
--- WiinUSoft/trunk/WiinUSoft/MainWindow.xaml.cs
+++ WiinUSoft/trunk/WiinUSoft/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
 
 namespace WiinUSoft
 {
@@ -19,9 +20,86 @@ namespace WiinUSoft
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Point dragStart;
         public MainWindow()
         {
             InitializeComponent();
+
+            DragCanvas.SetCanBeDragged(dest, false);
         }
+
+        private void Rectangle_DragEnter(object sender, DragEventArgs e)
+        {
+            // When the mouse is over this control while dragging
+            Debug.WriteLine("Drag Enter");
+        }
+
+        private void Rectangle_Drop(object sender, DragEventArgs e)
+        {
+            // When dragging is dropped/stopped over this control
+            Debug.WriteLine("Dropped");
+        }
+
+        private void Rectangle_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // When the mouse button is clicked on this control
+            Debug.WriteLine("Mouse Left Down");
+            dragStart = e.GetPosition(null);
+        }
+
+        private void Rectangle_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            // when the mouse is moving over this control before dragging has started
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragDrop.DoDragDrop(sender as Rectangle, "test", DragDropEffects.Move);
+            } 
+        }
+
+        private void Rectangle_GiveFeedback(object sender, GiveFeedbackEventArgs e)
+        {
+            // Called while dragging this ojbect
+            Debug.WriteLine("dragging");
+            Debug.WriteLine(Mouse.GetPosition(null));
+        }
+
+        private void Rectangle_QueryContinueDrag(object sender, QueryContinueDragEventArgs e)
+        {
+            Debug.WriteLine("Continue Dragging");
+            Debug.WriteLine(Mouse.GetPosition(null));
+        }
+
+
+
+
+        private void testRect_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            Debug.WriteLine("Got Mouse Capture");
+        }
+
+        private bool dragging = false;
+        private void testRect_IsMouseCapturedChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            Debug.WriteLine("Mouse Capture state changed: " + e.NewValue.ToString());
+
+            dragging = (bool)e.NewValue;
+
+            Debug.WriteLine(dragging);
+            if (!dragging)
+            {
+                DragCanvas.SetLeft((UIElement)sender, 0);
+            }
+        }
+
+        private void testRect_IsMouseCaptureWithinChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            Debug.WriteLine("Mouse Capture Within state changed: " + e.NewValue.ToString());
+        }
+
+        private void testRect_IsMouseDirectlyOverChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            Debug.WriteLine("mouse over");
+        }
+
     }
 }
