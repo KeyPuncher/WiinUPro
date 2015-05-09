b38787e2e8022a7d87d26c9159afba7b056568d7
jkeys <jkeys@367c74e7-bc52-1c41-9f2d-32f4c5dc80c3>
2015-05-09 01:49:08 +0000

    Tagged v1.1

---
+++ WiinUSoft/tags/Release_v1.1/WiinUSoft/PropWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WiinUSoft
+{
+    /// <summary>
+    /// Interaction logic for PropWindow.xaml
+    /// </summary>
+    public partial class PropWindow : Window
+    {
+        // TODO: connect to specific device
+        public bool doSave = false;
+        public Property props;
+
+        public PropWindow(Property org, string defalutName = "")
+        {
+            InitializeComponent();
+
+            props = new Property(org);
+            nameInput.Text = string.IsNullOrWhiteSpace(props.name) ? defalutName : props.name;
+            defaultInput.Text = props.profile;
+            autoCheckbox.IsChecked = props.autoConnect;
+            rumbleEnabled.IsChecked = props.useRumble;
+            if (props.autoNum >= 0 && props.autoNum <= autoConnectNumber.Items.Count)
+            {
+                autoConnectNumber.SelectedIndex = props.autoNum;
+            }
+            if (props.rumbleIntensity >= 0 && props.rumbleIntensity <= rumbleSelection.Items.Count)
+            {
+                rumbleSelection.SelectedIndex = props.rumbleIntensity;
+            }
+        }
+
+        private void cancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void saveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            doSave = true;
+            Close();
+        }
+
+        private void autoCheckbox_Click(object sender, RoutedEventArgs e)
+        {
+            props.autoConnect = autoCheckbox.IsChecked == true;
+        }
+
+        private void rumbleEnabled_Click(object sender, RoutedEventArgs e)
+        {
+            props.useRumble = rumbleEnabled.IsChecked == true;
+        }
+
+        private void nameInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            props.name = nameInput.Text;
+        }
+
+        private void defaultInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            props.profile = defaultInput.Text;
+        }
+
+        private void defaultBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.DefaultExt = ".wsp";
+            dialog.Filter = App.PROFILE_FILTER;
+
+            Nullable<bool> doLoad = dialog.ShowDialog();
+
+            if (doLoad == true && dialog.CheckFileExists)
+            {
+                defaultInput.Text = dialog.FileName;
+            }
+        }
+
+        private void AutoConnect_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (props != null)
+            {
+                props.autoConnect = autoConnectNumber.SelectedIndex > 0;
+                props.autoNum = autoConnectNumber.SelectedIndex;
+            }
+        }
+
+        private void Rumble_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (props != null)
+            {
+                props.useRumble = rumbleSelection.SelectedIndex > 0;
+                props.rumbleIntensity = rumbleSelection.SelectedIndex;
+            }
+        }
+    }
+}
---
+++ WiinUSoft/tags/Release_v1.1/WiinUSoft/UserPrefs.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace WiinUSoft
+{
+    public class UserPrefs
+    {
+        private static UserPrefs _instance;
+        public static UserPrefs Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + @"\prefs.config"))
+                    {
+                        LoadPrefs();
+                    }
+                    else
+                    {
+                        _instance = new UserPrefs();
+                        _instance.devicePrefs = new List<Property>();
+                        _instance.defaultProfile = new Profile();
+                        SavePrefs();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        public List<Property> devicePrefs;
+        public Profile defaultProfile;
+
+        public UserPrefs()
+        { }
+
+        public static bool LoadPrefs()
+        {
+            bool successful = false;
+            XmlSerializer X = new XmlSerializer(typeof(UserPrefs));
+            
+            try
+            {
+                if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + @"\prefs.config"))
+                {
+                    using (FileStream stream = File.OpenRead(AppDomain.CurrentDomain.BaseDirectory + @"\prefs.config"))
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        _instance = X.Deserialize(reader) as UserPrefs;
+                        reader.Close();
+                        stream.Close();
+                    }
+
+                    successful = true;
+                }
+            }
+            catch (Exception e) 
+            {
+                System.Diagnostics.Debug.WriteLine(e.Message);
+            }
+
+            return successful;
+        }
+
+        public static void SavePrefs()
+        {
+            XmlSerializer X = new XmlSerializer(typeof(UserPrefs));
+            string path = AppDomain.CurrentDomain.BaseDirectory + @"\prefs.config";
+
+            if (File.Exists(path))
+            {
+                FileInfo prefs = new FileInfo(path);
+                using (FileStream stream = File.Open(path, FileMode.Create, FileAccess.ReadWrite))
+                using (StreamWriter writer = new StreamWriter(stream))
+                {
+                    X.Serialize(writer, _instance);
+                    writer.Close();
+                    stream.Close();
+                }
+            }
+            else
+            {
+                using (FileStream stream = File.Create(path))
+                using (StreamWriter writer = new StreamWriter(stream))
+                {
+                    X.Serialize(writer, _instance);
+                    writer.Close();
+                    stream.Close();
+                }
+            }
+        }
+
+        public Property GetDevicePref(string hid)
+        {
+            foreach (var pref in devicePrefs)
+            {
+                if (pref.hid == hid)
+                {
+                    return pref;
+                }
+            }
+
+            return null;
+        }
+
+        public void AddDevicePref(Property property)
+        {
+            foreach (var pref in devicePrefs)
+            {
+                if (pref.hid == property.hid)
+                {
+                    pref.name = property.name;
+                    pref.autoConnect = property.autoConnect;
+                    pref.profile = property.profile;
+                    pref.connType = property.connType;
+                    pref.autoNum = property.autoNum;
+                    pref.rumbleIntensity = property.rumbleIntensity;
+                    pref.useRumble = property.useRumble;
+
+                    return;
+                }
+            }
+
+            devicePrefs.Add(property);
+        }
+    }
+
+    public class Property
+    {
+        public enum ProfHolderType
+        {
+            XInput = 0,
+            DInput = 1
+        }
+
+        public string hid = "";
+        public string name = "";
+        public bool autoConnect = false;
+        public bool useRumble = true;
+        public int autoNum = 0;
+        public int rumbleIntensity = 2;
+        public ProfHolderType connType;
+        public string profile = "";
+
+        public Property()
+        {
+            hid = "";
+            connType = ProfHolderType.XInput;
+        }
+
+        public Property(string ID)
+        {
+            hid = ID;
+            connType = ProfHolderType.XInput;
+        }
+
+        public Property(Property copy)
+        {
+            hid = copy.hid;
+            name = copy.name;
+            autoConnect = copy.autoConnect;
+            autoNum = copy.autoNum;
+            useRumble = copy.useRumble;
+            rumbleIntensity = copy.rumbleIntensity;
+            connType = copy.connType;
+            profile = copy.profile;
+        }
+    }
+
+    public class Profile
+    {
+        public enum HolderType
+        {
+            XInput = 0,
+            DInput = 1
+        }
+
+        public NintrollerLib.ControllerType profileType;
+        public HolderType connType;
+        public List<string> controllerMapKeys;
+        public List<string> controllerMapValues;
+
+        public Profile()
+        {
+            profileType = NintrollerLib.ControllerType.Wiimote;
+            controllerMapKeys = new List<string>();
+            controllerMapValues = new List<string>();
+            connType = HolderType.XInput;
+        }
+
+        public Profile(NintrollerLib.ControllerType type)
+        {
+            profileType = type;
+            controllerMapKeys = new List<string>();
+            controllerMapValues = new List<string>();
+            connType = HolderType.XInput;
+        }
+    }
+
+}
