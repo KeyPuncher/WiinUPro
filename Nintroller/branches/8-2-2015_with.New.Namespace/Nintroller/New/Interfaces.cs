33f6421c3b35d1d6f93170efb91b05a5340a117b
Justin <Justin@367c74e7-bc52-1c41-9f2d-32f4c5dc80c3>
2015-08-02 22:31:13 +0000

    Created branch before removing the New namespace that was used during the Nintroller library re-write.

---
+++ Nintroller/branches/8-2-2015_with.New.Namespace/Nintroller/New/Interfaces.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NintrollerLib.New
+{
+    public interface INintrollerState
+    {
+        void Update(byte[] data);
+        float GetValue(string input);
+        void SetCalibration(Calibrations.CalibrationPreset preset);
+        void SetCalibration(INintrollerState from);
+        void SetCalibration(string calibrationString);
+        string GetCalibrationString();
+        bool CalibrationEmpty { get; }
+    }
+
+    public interface INintrollerParsable
+    {
+        void Parse(byte[] input, int offset = 0);
+    }
+
+    public interface INintrollerNormalizable
+    {
+        void Normalize();
+    }
+
+    public interface INintrollerBounds
+    {
+        bool InBounds(int x, int y = 0, int z = 0);
+    }
+}
