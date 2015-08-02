484863400646492fbb3ef9d495360973895dad32
Justin <Justin@367c74e7-bc52-1c41-9f2d-32f4c5dc80c3>
2015-08-02 22:32:45 +0000

    Tagged WiinUSoft before removing the Nintroller.New namespace.

---
+++ WiinUSoft/tags/v2_PreRelease/WiinUSoft/Inputs/ProController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WiinUSoft.Inputs
+{
+    public class ProController
+    {
+        public const string A      = "proA";
+        public const string B      = "proB";
+        public const string X      = "proX";
+        public const string Y      = "proY";
+
+        public const string UP     = "proUP";
+        public const string DOWN   = "proDOWN";
+        public const string LEFT   = "proLEFT";
+        public const string RIGHT  = "proRIGHT";
+                                      
+        public const string L      = "proL";
+        public const string R      = "proR";
+        public const string ZL     = "proZL";
+        public const string ZR     = "proZR";
+                                      
+        //public const string LX   = "LX";
+        //public const string LY   = "LY";
+        //public const string RX   = "RX";
+        //public const string RY   = "RY";
+                                      
+        public const string LUP    = "proLUP";
+        public const string LDOWN  = "proLDOWN";
+        public const string LLEFT  = "proLLEFT";
+        public const string LRIGHT = "proLRIGHT";
+                                      
+        public const string RUP    = "proRUP";
+        public const string RDOWN  = "proRDOWN";
+        public const string RLEFT  = "proRLEFT";
+        public const string RRIGHT = "proRRIGHT";
+                                      
+        public const string LS     = "proLS";
+        public const string RS     = "proRS";
+                                      
+        public const string SELECT = "proSELECT";
+        public const string START  = "proSTART";
+        public const string HOME   = "proHOME";
+    }
+}
---
+++ WiinUSoft/tags/v2_PreRelease/WiinUSoft/Inputs/Wiimote.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WiinUSoft.Inputs
+{
+    public static class Wiimote
+    {
+        public const string A           = "wA";
+        public const string B           = "wB";
+        public const string ONE         = "wONE";
+        public const string TWO         = "wTWO";
+
+        // dpad when wiimote is vertical
+        public const string UP          = "wUP";
+        public const string DOWN        = "wDOWN";
+        public const string LEFT        = "wLEFT";
+        public const string RIGHT       = "wRIGHT";
+
+        public const string MINUS       = "wMINUS";
+        public const string PLUS        = "wPLUS";
+        public const string HOME        = "wHOME";
+
+        // tilting the controler with the wrist
+        public const string TILT_RIGHT  = "wTILTRIGHT";
+        public const string TILT_LEFT   = "wTILTLEFT";
+        public const string TILT_UP     = "wTILTUP";
+        public const string TILT_DOWN   = "wTILTDOWN";
+        public const string FACE_UP     = "wTILTFACEUP";
+        public const string FACE_DOWN   = "wTILTFACEDOWN";
+
+        // for quick movement
+        public const string ACC_UP       = "wACCUP";
+        public const string ACC_DOWN     = "wACCDOWN";
+        public const string ACC_LEFT     = "wACCLEFT";
+        public const string ACC_RIGHT    = "wACCRIGHT";
+        public const string ACC_FORWARD  = "wACCFORWARD";
+        public const string ACC_BACKWARD = "wACCBACKWARD";
+        public const string ACC_SHAKE_X  = "wACCSHAKEX";
+        public const string ACC_SHAKE_Y  = "wACCSHAKEY";
+        public const string ACC_SHAKE_Z  = "wACCSHAKEZ";
+
+        // Pointer from IR camera
+        public const string IR_X         = "wIRX";
+        public const string IR_Y         = "wIRY";
+
+        // IR Pointer like a joystick
+        public const string IR_UP        = "wIRUP";
+        public const string IR_DOWN      = "wIRDOWN";
+        public const string IR_LEFT      = "wIRLEFT";
+        public const string IR_RIGHT     = "wIRRIGHT";
+    }
+}
