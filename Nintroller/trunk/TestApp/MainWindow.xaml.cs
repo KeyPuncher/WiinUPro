f8815f0a8162c4641e3732aba7551b6d76f389d5
Justin <Justin@367c74e7-bc52-1c41-9f2d-32f4c5dc80c3>
2015-08-02 22:46:17 +0000

    Removed New Namespace

--- Nintroller/trunk/TestApp/MainWindow.xaml.cs
+++ Nintroller/trunk/TestApp/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-using NintrollerLib.New;
+using NintrollerLib;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 
