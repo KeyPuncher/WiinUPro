a6a5a29ac98da8cad27ed3b6409aa6660a7e5fcd
justinkeys <justinkeys@367c74e7-bc52-1c41-9f2d-32f4c5dc80c3>
2015-09-04 23:05:40 +0000

    Setting up files for WiinUPro

---
+++ trunk/WiinUPro/Directors/MouseDirector.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiinUPro
+{
+    class MouseDirector
+    {
+        public static MouseDirector Access { get; protected set; }
+
+        static MouseDirector()
+        {
+            Access = new MouseDirector();
+        }
+
+        public MouseDirector()
+        {
+            // TODO Director: Initilize mouse hook
+        }
+
+        public void MouseButtonDown(int code)
+        {
+            // TODO Director: mouse btn down
+        }
+
+        public void MouseButtonUp(int code)
+        {
+            // TODO Director: mouse btn up
+        }
+
+        public void MouseButtonPress(int code)
+        {
+            // TODO Director: mouse btn press
+        }
+
+        public void MouseMoveX(float amount)
+        {
+            // TODO Director: mouse movement along X plane
+        }
+
+        public void MouseMoveY(float amount)
+        {
+            // TODO Director: mouse movement along Y plane
+        }
+
+        public void MouseMoveTo(float x, float y)
+        {
+            // TODO Director: absolute mouse movement
+            //System.Windows.SystemParameters.PrimaryScreenHeight;
+            //System.Windows.SystemParameters.PrimaryScreenWidth;
+        }
+    }
+}
---
+++ trunk/WiinUPro/Directors/VJoyDirector.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiinUPro
+{
+    class VJoyDirector
+    {
+        public static VJoyDirector Access { get; protected set; }
+
+        static VJoyDirector()
+        {
+            Access = new VJoyDirector();
+        }
+
+        public VJoyDirector()
+        {
+            // TODO Director: Initilize vJoy
+        }
+
+        public bool Available { get; protected set; }
+
+        public int ControllerCount { get; protected set; }
+    }
+}
