28f96beabc327d9e4306d2738a473fe08e36934d
justinkeys <justinkeys@367c74e7-bc52-1c41-9f2d-32f4c5dc80c3>
2015-09-08 23:35:18 +0000

    Created version 2.1 release branch

---
+++ tags/WiinUSoft/Release_v2.1/Inputs/Xbox360.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WiinUSoft.Inputs
+{
+    public static class Xbox360
+    {
+        public const string A      = "xA";
+        public const string B      = "xB";
+        public const string X      = "xX";
+        public const string Y      = "xY";
+
+        public const string UP     = "xUP";
+        public const string DOWN   = "xDOWN";
+        public const string LEFT   = "xLEFT";
+        public const string RIGHT  = "xRIGHT";
+
+        public const string LB     = "xLB";
+        public const string RB     = "xRB";
+        public const string LT     = "xLT";
+        public const string RT     = "xRT";
+
+        //public const string LX   = "LX";
+        //public const string LY   = "LY";
+        //public const string RX   = "RX";
+        //public const string RY   = "RY";
+
+        public const string LUP    = "xLUP";
+        public const string LDOWN  = "xLDOWN";
+        public const string LLEFT  = "xLLEFT";
+        public const string LRIGHT = "xLRIGHT";
+
+        public const string RUP    = "xRUP";
+        public const string RDOWN  = "xRDOWN";
+        public const string RLEFT  = "xRLEFT";
+        public const string RRIGHT = "xRRIGHT";
+
+        public const string LS     = "xLS";
+        public const string RS     = "xRS";
+
+        public const string BACK   = "xBACK";
+        public const string START  = "xSTART";
+        public const string GUIDE  = "xGUIDE";
+    }
+}
---
+++ tags/WiinUSoft/Release_v2.1/Windows/ErrorWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WiinUSoft
+{
+    /// <summary>
+    /// Interaction logic for ErrorWindow.xaml
+    /// </summary>
+    public partial class ErrorWindow : Window
+    {
+        private Exception _exception;
+
+        public ErrorWindow()
+        {
+            InitializeComponent();
+        }
+
+        public ErrorWindow(Exception ex)
+            : this()
+        {
+            _exception = ex;
+
+            _errorMessage.Content = ex.Message;
+            _errorStack.Text = ex.StackTrace;
+        }
+
+        private void _dontSendBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void _sendBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+
+            // Option 1, send an email
+            System.Net.Mail.MailMessage message = new System.Net.Mail.MailMessage();
+            message.To.Add("justin@wiinupro.com");
+            message.Subject = "WiinUSoft Error";
+            message.From = new System.Net.Mail.MailAddress("justin@wiinupro.com");
+            message.Body = _exception.InnerException != null
+                ? string.Format("Date: {2}\n\nOS: {3}\n\nUser Comments: {6}\n\nMessage: {0}\n\nStack: {1}\n\nInner Message: {4}\n\nInnerStack:\n {5}",
+                    _exception.Message,                     // 0
+                    _exception.StackTrace,                  // 1
+                    System.DateTime.Now,                    // 2
+                    Environment.OSVersion.ToString(),       // 3
+                    _exception.InnerException.Message,      // 4
+                    _exception.InnerException.StackTrace,   // 5
+                    _userInfo.Text)                         // 6
+                : string.Format("Date: {2}\n\nOS: {3}\n\nUser Comments: {4}\n\nMessage: {0}\n\nStack:\n {1}",
+                    _exception.Message,                     // 0
+                    _exception.StackTrace,                  // 1
+                    System.DateTime.Now,                    // 2
+                    Environment.OSVersion.ToString(),       // 3
+                    _userInfo.Text);                        // 4
+
+            System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.Credentials = new System.Net.NetworkCredential("wiinuproError@gmail.com", "wiinupro101");
+            smtp.EnableSsl = true;
+            smtp.Timeout = 30;
+
+            try
+            {
+                smtp.Send(message);
+            }
+            catch
+            {
+                // it's okay if we fail, we just won't notify the user
+            }
+            
+            // Option 2, post to server
+        }
+    }
+}
