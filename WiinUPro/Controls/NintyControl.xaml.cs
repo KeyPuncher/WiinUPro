53a28f0234896d356a9501a21c4b8b347746c35e
justinkeys <KeyPuncher@users.noreply.github.com>
2015-10-09 22:58:53 +0000

    Added visual update code for Pro Controller. Added NintyControl to manage devices. Added Test code that adds a connected pro to the tab control.

---
+++ WiinUPro/Controls/NintyControl.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NintrollerLib;
+
+namespace WiinUPro
+{
+    /// <summary>
+    /// Interaction logic for NintyControl.xaml
+    /// </summary>
+    public partial class NintyControl : UserControl
+    {
+        internal Nintroller _nintroller;
+
+        public NintyControl()
+        {
+            InitializeComponent();
+        }
+
+        public NintyControl(string devicePath) : this()
+        {
+            _nintroller = new Nintroller(devicePath);
+            _view.Child = new ProControl();
+            ((UserControl)_view.Child).HorizontalAlignment = HorizontalAlignment.Left;
+            ((UserControl)_view.Child).VerticalAlignment = VerticalAlignment.Top;
+        }
+    }
+
+    public interface INintyControl
+    {
+        void UpdateVisual(INintrollerState state);
+    }
+}
--- WiinUPro/Controls/ProControl.xaml.cs
+++ WiinUPro/Controls/ProControl.xaml.cs
@@ -12,17 +12,50 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using NintrollerLib;
 
 namespace WiinUPro
 {
     /// <summary>
     /// Interaction logic for ProControl.xaml
     /// </summary>
-    public partial class ProControl : UserControl
+    public partial class ProControl : UserControl, INintyControl
     {
         public ProControl()
         {
             InitializeComponent();
+        }
+
+        public void UpdateVisual(INintrollerState state)
+        {
+            if (state.GetType() == typeof(ProController))
+            {
+                var pro = (ProController)state;
+
+                aBtn.Opacity = pro.A ? 1 : 0;
+                aBtn.Opacity = pro.B ? 1 : 0;
+                xBtn.Opacity = pro.X ? 1 : 0;
+                yBtn.Opacity = pro.Y ? 1 : 0;
+                lBtn.Opacity = pro.L ? 1 : 0;
+                rBtn.Opacity = pro.R ? 1 : 0;
+                zlBtn.Opacity = pro.ZL ? 1 : 0;
+                zrBtn.Opacity = pro.ZR ? 1 : 0;
+                dpadUp.Opacity = pro.Up ? 1 : 0;
+                dpadDown.Opacity = pro.Down ? 1 : 0;
+                dpadLeft.Opacity = pro.Left ? 1 : 0;
+                dpadRight.Opacity = pro.Right ? 1 : 0;
+                dpadCenter.Opacity = (pro.Up || pro.Down || pro.Left || pro.Right) ? 1 : 0;
+                homeBtn.Opacity = pro.Home ? 1 : 0;
+                plusBtn.Opacity = pro.Plus ? 1 : 0;
+                minusBtn.Opacity = pro.Minus ? 1 : 0;
+                leftStickBtn.Opacity = pro.LStick ? 1 : 0;
+                rightStickBtn.Opacity = pro.RStick ? 1 : 0;
+
+                leftStick.Margin = new Thickness(196 + 50 * pro.LJoy.X, 232 + 50 * pro.LJoy.Y, 0, 0);
+                leftStickBtn.Margin = new Thickness(196 + 50 * pro.LJoy.X, 230 + 50 * pro.LJoy.Y, 0, 0);
+                rightStick.Margin = new Thickness(980 + 50 * pro.RJoy.X, 232 + 50 * pro.RJoy.Y, 0, 0);
+                rightStickBtn.Margin = new Thickness(980 + 50 * pro.RJoy.X, 230 + 50 * pro.RJoy.Y, 0, 0);
+            }
         }
     }
 }
