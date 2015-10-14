0bf1c0e84e87980cd6c42bf3666e21d93686a604
justinkeys <KeyPuncher@users.noreply.github.com>
2015-10-14 23:06:48 +0000

    Modified where the control gets created when connecting.

--- WiinUPro/Controls/NintyControl.xaml.cs
+++ WiinUPro/Controls/NintyControl.xaml.cs
@@ -46,6 +46,41 @@ namespace WiinUPro
         private void _nintroller_ExtensionChange(object sender, NintrollerExtensionEventArgs e)
         {
             // Handle the extension change
+            Dispatcher.Invoke(new Action(() =>
+            {
+                bool success = false;
+
+                switch (e.controllerType)
+                {
+                    case ControllerType.ProController:
+                        _controller = new ProControl();
+                        break;
+                }
+
+                if (_controller != null)
+                {
+                    _controller.ChangeLEDs(_nintroller.Led1, _nintroller.Led2, _nintroller.Led3, _nintroller.Led4);
+                    _controller.OnChangeLEDs += SetLeds;
+                    _controller.OnInputSelected += InputSelected;
+                    _controller.OnInputRightClick += InputOpenMenu;
+
+                    _view.Child = _controller as UserControl;
+                    ((UserControl)_view.Child).HorizontalAlignment = HorizontalAlignment.Left;
+                    ((UserControl)_view.Child).VerticalAlignment = VerticalAlignment.Top;
+
+                    success = true;
+
+                    if (success)
+                    {
+                        btnDisconnect.IsEnabled = true;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Device or Extension not supported.");
+                        //btnConnect.IsEnabled = true;
+                    }
+                }
+            }));
         }
 
         private void _nintroller_StateUpdate(object sender, NintrollerStateEventArgs e)
@@ -64,7 +99,14 @@ namespace WiinUPro
         #region GUI Events
         private void btnConnect_Click(object sender, RoutedEventArgs e)
         {
-            bool success = false;
+            // Note: Code below was commented out because it was moved to Extension Change event
+            // The idea here is that we change controls when the device type is discovered or changed
+            // For Wiimote extensions, the type should be passed to the WiimoteControl.
+            // If connection fails we inform the user and re-enable the Connect button.
+            // If the controller type is unknown or not supported, the user has the option to
+            // disconnect from the controller and the control element should look disabled
+
+            //bool success = false;
 
             if (_nintroller.Connect())
             {
@@ -77,10 +119,10 @@ namespace WiinUPro
                 //switch (_nintroller.Type)
                 //{
                 //    case ControllerType.ProController:
-                        _controller = new ProControl();
+                //        _controller = new ProControl();
                 //        break;
                 //}
-
+                /*
                 if (_controller != null)
                 {
                     _controller.ChangeLEDs(_nintroller.Led1, _nintroller.Led2, _nintroller.Led3, _nintroller.Led4);
@@ -93,7 +135,8 @@ namespace WiinUPro
                     ((UserControl)_view.Child).VerticalAlignment = VerticalAlignment.Top;
 
                     success = true;
-                }
+                }*/
+                btnDisconnect.IsEnabled = true;
             }
 #if DEBUG
             else
@@ -104,19 +147,26 @@ namespace WiinUPro
                 _view.Child = _controller as UserControl;
                 ((UserControl)_view.Child).HorizontalAlignment = HorizontalAlignment.Left;
                 ((UserControl)_view.Child).VerticalAlignment = VerticalAlignment.Top;
-                success = true;
-            }
-#endif
-
-            if (success)
-            {
+                //success = true;
                 btnDisconnect.IsEnabled = true;
             }
+#else
             else
             {
                 MessageBox.Show("Could not connect to device!");
                 btnConnect.IsEnabled = true;
             }
+#endif
+
+            //if (success)
+            //{
+            //    btnDisconnect.IsEnabled = true;
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Could not connect to device!");
+            //    btnConnect.IsEnabled = true;
+            //}
         }
 
         private void btnDisconnect_Click(object sender, RoutedEventArgs e)
