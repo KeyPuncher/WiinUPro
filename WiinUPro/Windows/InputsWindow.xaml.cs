597263878bb14fe7339045ab73224e677bb3fa79
justinkeys <KeyPuncher@users.noreply.github.com>
2015-10-17 00:38:24 +0000

    Created KeyboardAssignment class

---
+++ WiinUPro/Windows/InputsWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WiinUPro
+{
+    /// <summary>
+    /// Interaction logic for InputsWindow.xaml
+    /// </summary>
+    public partial class InputsWindow : Window
+    {
+        public AssignmentCollection Result { get; protected set; }
+
+        public InputsWindow()
+        {
+            InitializeComponent();
+            Result = new AssignmentCollection();
+        }
+
+        public InputsWindow(AssignmentCollection collection) : this()
+        {
+            // fill the window with current assignments
+        }
+    }
+}
