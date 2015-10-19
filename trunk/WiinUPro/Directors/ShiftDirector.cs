13c1ef1bd746661f4fec3d1e17d42762d5fc9f27
justinkeys <justinkeys@367c74e7-bc52-1c41-9f2d-32f4c5dc80c3>
2015-10-19 23:22:20 +0000

    Added ShiftDirector and ShiftAssignment classes

---
+++ trunk/WiinUPro/Directors/ShiftDirector.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiinUPro
+{
+    static class ShiftDirector
+    {
+        static ShiftDirector()
+        {
+            _currentState = ShiftState.None;
+        }
+
+        public static int CurrentShiftState
+        {
+            get
+            {
+                return (int)_currentState;
+            }
+        }
+
+        private static ShiftState _currentState;
+
+        public static void ChangeState(ShiftState newState)
+        {
+            if (_currentState != newState)
+            {
+                KeyboardDirector.Access.Release();
+                MouseDirector.Access.Release();
+                _currentState = newState;
+            }
+        }
+    }
+
+    public enum ShiftState
+    {
+        None = 0,
+        Red = 1,
+        Blue = 2,
+        Green = 3
+    }
+}
