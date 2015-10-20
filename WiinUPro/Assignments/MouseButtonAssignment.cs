82b8324e70da69adf5321243a11fcb21c5435952
justinkeys <KeyPuncher@users.noreply.github.com>
2015-10-20 21:12:14 +0000

    Add shell for MouseButtonAssignment

---
+++ WiinUPro/Assignments/MouseButtonAssignment.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiinUPro.Assignments
+{
+    public class MouseButtonAssignment : IAssignment
+    {
+        public void Apply(float value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SameAs(IAssignment assignment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
