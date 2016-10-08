a7e0f8762c7810ceba216ef35782c9ddf86f44ed
justinkeys <justinkeys@367c74e7-bc52-1c41-9f2d-32f4c5dc80c3>
2016-10-08 16:51:12 +0000

    Adding un-commited files.

---
+++ trunk/WiinUPro/Assignments/XInputAxisAssignment.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScpControl;
+
+namespace WiinUPro
+{
+    public class XInputAxisAssignment : IAssignment
+    {
+        /// <summary>
+        /// The XInput Device to use
+        /// </summary>
+        public ScpDirector.XInput_Device Device { get; set; }
+
+        /// <summary>
+        /// The XInput Axis to be simulated.
+        /// </summary>
+        public X360Axis Axis { get; set; }
+
+        public XInputAxisAssignment(X360Axis axis)
+        {
+            Axis = axis;
+        }
+
+        public void Apply(float value)
+        {
+            ScpDirector.Access.SetAxis(Axis, value, Device);
+        }
+
+        public bool SameAs(IAssignment assignment)
+        {
+            var other = assignment as XInputAxisAssignment;
+
+            if (other == null)
+            {
+                return false;
+            }
+
+            bool result = true;
+
+            result &= Axis == other.Axis;
+            result &= Device == other.Device;
+
+            return result;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as XInputAxisAssignment;
+
+            if (other == null)
+            {
+                return false;
+            }
+            else
+            {
+                return Axis == other.Axis && Device == other.Device;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = (int)Axis + (int)Device;
+            return hash;
+        }
+
+        public override string ToString()
+        {
+            return Axis.ToString();
+        }
+    }
+}
---
+++ trunk/WiinUPro/Windows/DummyWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using NintrollerLib;
+using Shared;
+
+namespace WiinUPro.Windows
+{
+    /// <summary>
+    /// Interaction logic for DummyWindow.xaml
+    /// </summary>
+    public partial class DummyWindow : Window
+    {
+        public DummyDevice Device { get; protected set; }
+        public ControllerType Type { get; set; }
+
+        bool isWiimote { get { return Device.State is Wiimote; } }
+        bool isPro { get { return Device.State is ProController; } }
+
+        public DummyWindow(DummyDevice device)
+        {
+            InitializeComponent();
+            Device = device;
+        }
+
+        public void SwitchType(ControllerType newType)
+        {
+            switch (newType)
+            {
+                case ControllerType.Wiimote:
+                    Device.State = new Wiimote();
+                    groupCore.Visibility = Visibility.Visible;
+                    break;
+
+                case ControllerType.ProController:
+                    Device.State = new ProController();
+                    groupCore.Visibility = Visibility.Visible;
+                    break;
+            }
+        }
+
+        private ProController ChangeProBoolean(string property)
+        {
+            ProController pro = (ProController)Device.State;
+
+            switch (property)
+            {
+                case INPUT_NAMES.PRO_CONTROLLER.A     : pro.A      = !pro.A; break;
+                case INPUT_NAMES.PRO_CONTROLLER.B     : pro.B      = !pro.B; break;
+                case INPUT_NAMES.PRO_CONTROLLER.X     : pro.X      = !pro.X; break;
+                case INPUT_NAMES.PRO_CONTROLLER.Y     : pro.Y      = !pro.Y; break;
+                case INPUT_NAMES.PRO_CONTROLLER.L     : pro.L      = !pro.L; break;
+                case INPUT_NAMES.PRO_CONTROLLER.R     : pro.R      = !pro.R; break;
+                case INPUT_NAMES.PRO_CONTROLLER.ZL    : pro.ZL     = !pro.ZL; break;
+                case INPUT_NAMES.PRO_CONTROLLER.ZR    : pro.ZR     = !pro.ZR; break;
+                case INPUT_NAMES.PRO_CONTROLLER.UP    : pro.Up     = !pro.Up; break;
+                case INPUT_NAMES.PRO_CONTROLLER.DOWN  : pro.Down   = !pro.Down; break;
+                case INPUT_NAMES.PRO_CONTROLLER.LEFT  : pro.Left   = !pro.Left; break;
+                case INPUT_NAMES.PRO_CONTROLLER.RIGHT : pro.Right  = !pro.Right; break;
+                case INPUT_NAMES.PRO_CONTROLLER.START : pro.Start  = !pro.Start; break;
+                case INPUT_NAMES.PRO_CONTROLLER.SELECT: pro.Select = !pro.Select; break;
+                case INPUT_NAMES.PRO_CONTROLLER.HOME  : pro.Home   = !pro.Home; break;
+                case INPUT_NAMES.PRO_CONTROLLER.LS    : pro.LStick = !pro.LStick; break;
+                case INPUT_NAMES.PRO_CONTROLLER.RS    : pro.RStick = !pro.RStick; break;
+            }
+
+            return pro;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            string baseBtn = (sender as FrameworkElement).Tag.ToString();
+
+            if (isPro)
+            {
+                if (baseBtn == "MINUS") baseBtn = "SELECT";
+                if (baseBtn == "PLUS") baseBtn = "START";
+
+                Device.State = ChangeProBoolean("pro" + baseBtn);
+            }
+        }
+
+        private void ChangeProAnalog(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (isPro)
+            {
+                ProController pro = (ProController)Device.State;
+                float value = (float)Math.Round(e.NewValue, 2);
+
+                switch((sender as FrameworkElement).Tag.ToString())
+                {
+                    case "LX":
+                        pro.LJoy.X = value;
+                        pro.LJoy.rawX = CalculateRaw(pro.LJoy.minX, pro.LJoy.maxX, value);
+                        break;
+
+                    case "LY":
+                        pro.LJoy.Y = value;
+                        pro.LJoy.rawY = CalculateRaw(pro.LJoy.minY, pro.LJoy.maxY, value);
+                        break;
+
+                    case "RX":
+                        pro.RJoy.X = value;
+                        pro.RJoy.rawX = CalculateRaw(pro.RJoy.minX, pro.RJoy.maxX, value);
+                        break;
+
+                    case "RY":
+                        pro.RJoy.Y = value;
+                        pro.RJoy.rawY = CalculateRaw(pro.RJoy.minY, pro.RJoy.maxY, value);
+                        break;
+                }
+
+                Device.State = pro;
+            }
+        }
+
+        private short CalculateRaw(int min, int max, float value)
+        {
+            var raw = (max - min) * ((value + 1)/2f) + min;
+            return Convert.ToInt16(Math.Round(raw));
+        }
+    }
+}
