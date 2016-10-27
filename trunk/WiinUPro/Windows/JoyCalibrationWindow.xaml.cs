7ec6ba334383b4d614a5c6bac9b8c234bd0d7132
justinkeys <justinkeys@367c74e7-bc52-1c41-9f2d-32f4c5dc80c3>
2016-10-27 06:17:17 +0000

    Starting to add joystick calibration

---
+++ trunk/WiinUPro/Windows/JoyCalibrationWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using NintrollerLib;
+
+namespace WiinUPro.Windows
+{
+    /// <summary>
+    /// Interaction logic for JoyCalibrationWindow.xaml
+    /// </summary>
+    public partial class JoyCalibrationWindow : Window
+    {
+        protected short rawXLimitMax, rawXLimitMin, rawYLimitMax, rawYLimitMin;
+        protected short rawXDeadMax, rawXDeadMin, rawYDeadMax, rawYDeadMin;
+
+        protected Joystick _joystick;
+
+        public JoyCalibrationWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
