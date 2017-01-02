feede8751d1207c08484f657cf4599a325aa17d8
justinkeys <KeyPuncher@users.noreply.github.com>
2017-01-02 07:43:56 +0000

    Updated WiinUPro & Nintroller version numbers

--- WiinUPro/Properties/AssemblyInfo.cs
+++ WiinUPro/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WiinUPro")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("WiinUPro Preview")]
+[assembly: AssemblyDescription("Preview Build 1")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WiinUPro")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.4.1.354")]
+[assembly: AssemblyFileVersion("0.4.1.354")]
