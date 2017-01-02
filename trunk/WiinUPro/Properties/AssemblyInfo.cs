a3f7b81531ce707790f32c8e2afec69a58d407b4
justinkeys <justinkeys@367c74e7-bc52-1c41-9f2d-32f4c5dc80c3>
2017-01-02 07:45:19 +0000

    Removed old bool variable

--- trunk/WiinUPro/Properties/AssemblyInfo.cs
+++ trunk/WiinUPro/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.1.354")]
-[assembly: AssemblyFileVersion("0.4.1.354")]
+[assembly: AssemblyVersion("0.4.1.355")]
+[assembly: AssemblyFileVersion("0.4.1.355")]
