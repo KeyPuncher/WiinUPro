0353936e5a915d56d628e5314c954adb00c48fa1
justinkeys <justinkeys@367c74e7-bc52-1c41-9f2d-32f4c5dc80c3>
2017-02-16 05:34:20 +0000

    Added VJoy Assignment Classes

---
+++ trunk/WiinUPro/Assignments/VJoyAxisAssignment.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WiinUPro
+{
+    public class VJoyAxisAssignment : IAssignment
+    {
+        public uint DeviceId { get; set; }
+        public HID_USAGES Axis { get; set; }
+
+        public VJoyAxisAssignment() { }
+
+        public VJoyAxisAssignment(HID_USAGES axis, uint device = 1)
+        {
+            Axis = axis;
+            DeviceId = device;
+        }
+
+        public void Apply(float value)
+        {
+            VJoyDirector.Access.SetAxis(Axis, value, DeviceId);
+        }
+
+        public bool SameAs(IAssignment assignment)
+        {
+            var other = assignment as VJoyAxisAssignment;
+
+            if (other == null)
+            {
+                return false;
+            }
+
+            bool result = true;
+
+            result &= Axis == other.Axis;
+            result &= DeviceId == other.DeviceId;
+
+            return result;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as VJoyAxisAssignment;
+
+            if (other == null)
+            {
+                return false;
+            }
+            else
+            {
+                return Axis == other.Axis && DeviceId == other.DeviceId;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = (int)Axis + (int)DeviceId;
+            return hash;
+        }
+
+        public override string ToString()
+        {
+            return Axis.ToString();
+        }
+    }
+}
