9be6275b72a503e10c2a200f283c4a143aa3dae1
justinkeys <justinkeys@367c74e7-bc52-1c41-9f2d-32f4c5dc80c3>
2017-03-30 23:14:17 +0000

    Updated WiinUSoft Version

--- trunk/WiinUSoft/Properties/AssemblyInfo.cs
+++ trunk/WiinUSoft/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.1.463")]
-[assembly: AssemblyFileVersion("3.0.1.463")]
+[assembly: AssemblyVersion("3.1.3.466")]
+[assembly: AssemblyFileVersion("3.1.3.466")]
