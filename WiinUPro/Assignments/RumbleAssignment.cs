babaf0b4c3af092975a8a152dbcccf897a7cabd8
justinkeys <KeyPuncher@users.noreply.github.com>
2017-05-18 03:10:31 +0000

    Added Rumble on Press

---
+++ WiinUPro/Assignments/RumbleAssignment.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace WiinUPro
+{
+    public class RumbleAssignment : IAssignment
+    {
+        public Action<bool> Callback { get; protected set; }
+
+        private bool lastState = false;
+
+        public RumbleAssignment(Action<bool> callback)
+        {
+            Callback = callback;
+        }
+
+        public void Apply(float value)
+        {
+            bool newState = Math.Abs(value) > 0.1f;
+            
+            if (newState != lastState)
+            {
+                lastState = newState;
+                Callback(newState);
+            }
+        }
+
+        public bool SameAs(IAssignment assignment)
+        {
+            if (assignment is RumbleAssignment)
+            {
+                return Callback == (assignment as RumbleAssignment).Callback;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
